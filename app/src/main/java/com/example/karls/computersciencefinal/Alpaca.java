dc8b41800292bd343cb39d37e3ba949f63939515
karls <karlschmitt190@gmail.com>
2018-05-16 19:42:40 -0500

    Made the app work

--- app/src/main/java/com/example/karls/computersciencefinal/Alpaca.java
+++ app/src/main/java/com/example/karls/computersciencefinal/Alpaca.java
@@ -1,48 +1,13 @@
 package com.example.karls.computersciencefinal;
 
 public class Alpaca {
+    private int numOfAlpacas;
 
-
-    public Alpaca() {
-        this.typeOfAlpaca = 0;
-        this.amountOfAlpacas = 5;
-
+    public int getNumOfAlpacas() {
+        return numOfAlpacas;
     }
 
-    public void RangerAlpaca(){
-        this.typeOfAlpaca = 1;
-        this.amountOfAlpacas = 1;
-
+    public void setNumOfAlpacas(int numOfAlpacas) {
+        this.numOfAlpacas = numOfAlpacas;
     }
-
-    private int typeOfAlpaca; //0 is power ranger, 1 is regular
-
-    public int getTypeOfAlpaca() {
-        return typeOfAlpaca;
-    }
-
-    public void setTypeOfAlpaca(int typeOfAlpaca) {
-        this.typeOfAlpaca = typeOfAlpaca;
-    }
-
-    public int getAmountOfAlpacas() {
-        return amountOfAlpacas;
-    }
-
-    public void setAmountOfAlpacas(int amountOfAlpacas) {
-        this.amountOfAlpacas = amountOfAlpacas;
-    }
-
-    public int getStartingAmount() {
-        return startingAmount;
-    }
-
-    public void setStartingAmount(int startingAmount) {
-        this.startingAmount = startingAmount;
-    }
-
-    private int amountOfAlpacas;
-    private int startingAmount = 15;
-
-
 }
--- app/src/main/java/com/example/karls/computersciencefinal/MainActivity.java
+++ app/src/main/java/com/example/karls/computersciencefinal/MainActivity.java
@@ -2,12 +2,72 @@ package com.example.karls.computersciencefinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Alpaca theAlpacas = new Alpaca();
+
+    private int day = 0;
+    private int food = 15;
+
+    //Screen size
+
+    private double screenWidth;
+    private double screenHieght;
+
+    TextView numOfAlpacas;
+    TextView foodCount;
+    TextView dayCount;  //Textview for Daycount
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        Button eatButton = findViewById(R.id.eatButton);    //Creates eat button
+        Button newAlpaca = findViewById(R.id.newAlpaca);      //Creates New Alpaca button
+        Button sleepButton = findViewById(R.id.sleep);      //Creates Sleep button
+
+        this.dayCount = findViewById(R.id.dayCount);
+        this.numOfAlpacas = findViewById(R.id.numOfAlpacas);
+        this.foodCount = findViewById(R.id.food);
+
+        //Sleep Button Method
+        sleepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dayCount.setText("Day " + day++ );
+            }
+        });
+
+        //Eat Button
+        eatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int foodNeeded = theAlpacas.getNumOfAlpacas()/3; //5 if num of alpacas is 15
+                food = food-foodNeeded;
+                foodCount.setText(food + " Pieces of Food Remaining");
+
+
+            }
+        });
+
+
+
+
     }
-}
+
+
+
+
+
+
+    }
+
