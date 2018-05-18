77075d548b19b910531cd2405f62fc134f5136dd
KarlS <karlschmitt190@gmail.com>
2018-05-18 10:11:52 -0500

    In class work

--- app/src/main/java/com/example/karls/computersciencefinal/Alpaca.java
+++ app/src/main/java/com/example/karls/computersciencefinal/Alpaca.java
@@ -1,13 +1,38 @@
 package com.example.karls.computersciencefinal;
 
+
 public class Alpaca {
     private int numOfAlpacas = 15;
+    private int rareAlpacas;
+    private double Position =100;
+
+
+    public void setPosition(double position) {
+        Position = position;
+    }
+
+
+
+    public void numOfLlamasOnScreen(){
+
+    }
 
     public int getNumOfAlpacas() {
         return numOfAlpacas;
     }
 
     public void setNumOfAlpacas(int numOfAlpacas) {
-        this.numOfAlpacas = numOfAlpacas;
+        this.numOfAlpacas += numOfAlpacas;
     }
+
+    public int getRareAlpacas() {
+        return rareAlpacas;
+    }
+
+    public void setRareAlpacas(int rareAlpacas) {
+        this.rareAlpacas = rareAlpacas;
+    }
+
+    public double getPosition() {return Position;}
+
 }
--- app/src/main/java/com/example/karls/computersciencefinal/MainActivity.java
+++ app/src/main/java/com/example/karls/computersciencefinal/MainActivity.java
@@ -1,56 +1,76 @@
 package com.example.karls.computersciencefinal;
 
-import android.app.DialogFragment;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.graphics.Point;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+//PREVENT FOOD GOING INTO NEGATIVES BEFORE IT UPDATES
 
 public class MainActivity extends AppCompatActivity {
 
-    Alpaca theAlpacas = new Alpaca();
+
+    Random rand = new Random();
+    public void randomNum(){
+        int val = rand.nextInt(10);
+        randomVal = val;
+        int specialnum = rand.nextInt(100);
+        rareVal = specialnum;
+    }
+
+    Alpaca theAlpacas = new Alpaca();   //Creates Alpaca selection
 
     private int day = 0;
     private int food = 15;
+    private int randomVal;
+    private int rareVal;
+
+/////////////////////////////Movement///////////////////////////
+
+    private ImageView regularalpaca1;
+    private ImageView regularalpaca2;
+    private ImageView regularalpaca3;
+    private ImageView regularAlpaca4;
+    private ImageView rareAlpaca;
+
 
     //Screen size
 
     private double screenWidth;
     private double screenHieght;
 
-    TextView numOfAlpacas;
-    TextView foodCount;
+    //Position
+
+    private double regAlpaca1X;
+    private double regAlpacaY;
+
+    //Initalize Class
+
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
+
+
+    /////////////////////////////////////////////////////////
+
+    TextView numOfAlpacas;  //Textview for number of Alpacas
+    TextView foodCount; //Textview for food counter
     TextView dayCount;  //Textview for Daycount
-
-    public void showDialog() {
-        int mStackLevel=1;
-        mStackLevel++;
-
-        // DialogFragment.show() will take care of adding the fragment
-        // in a transaction.  We also want to remove any currently showing
-        // dialog, so make our own transaction and take care of that here.
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        // Create and show the dialog.
-        DialogFragment newFragment = MyDialogFragment.newInstance(mStackLevel);
-        newFragment.show(ft, "dialog");
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
         Button eatButton = findViewById(R.id.eatButton);    //Creates eat button
@@ -60,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         this.dayCount = findViewById(R.id.dayCount);
         this.numOfAlpacas = findViewById(R.id.numOfAlpacas);
         this.foodCount = findViewById(R.id.food);
+
 
         //Sleep Button Method
         sleepButton.setOnClickListener(new View.OnClickListener() {
@@ -78,8 +99,9 @@ public class MainActivity extends AppCompatActivity {
                     numOfAlpacas.setText("You are out of Alpacs");
                 }
 
-                if (food<=0)
+                else if (food<=0) {
                     foodCount.setText("You are out of food");
+                }
                 else {
                     int foodNeeded = theAlpacas.getNumOfAlpacas() / 3; //5 if num of alpacas is 15
                     food = food - foodNeeded;
@@ -89,6 +111,91 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        newAlpaca.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                randomNum();
+
+                if (food<5){
+                    numOfAlpacas.setText("Not enough food");
+                }
+                else if (randomVal > 7) {
+                    food = food -3;
+                    theAlpacas.setNumOfAlpacas(3);
+                    numOfAlpacas.setText(theAlpacas.getNumOfAlpacas() + " Alpacas");
+
+
+                } else if (randomVal>4 & randomVal<7) {
+                    food = food -3;
+                    theAlpacas.setNumOfAlpacas(1);
+                    numOfAlpacas.setText(theAlpacas.getNumOfAlpacas() + " Alpacas");
+                }
+
+                else{
+                    numOfAlpacas.setText(theAlpacas.getNumOfAlpacas() + " Alpacas");
+
+                }
+
+                if (rareVal==99){
+                    //Rare Llama, need to set up eventually
+                    theAlpacas.setNumOfAlpacas(+1);
+                    food = food -3;
+                }
+            }});
+
+        ///////////////////Movement//////////////////
+        regularalpaca1 = (ImageView) findViewById(R.id.regularalpaca1);
+
+        //Gets screen size
+
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+        screenWidth = size.x;
+        screenHieght = size.y;
+
+
+        //Timer Stuff
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changePos();
+                    }
+                });
+
+            }
+        },0,20);
+        screenHieght = screenHieght/1.5; //Might reduce travel
+
+
+
+
+
+
+
+
+
+    }
+    public void changePos(){
+
+        //Makes the penguin move up
+        //Also moves it side to side from each reset process
+        regAlpacaY -=3;//Controls speed of penguin, originally at 10
+        // x speed will be faster or slower based off of the number of pingus
+        if(regularalpaca1.getY() + regularalpaca1.getHeight() <0){
+            //regAlpaca1X = 10; //Keeps it in position x1
+            regAlpacaY = screenHieght + 20; //regAlpacaY = screenHieght + 100; //Used to be Y alpaca
+        }
+
+        regularalpaca1.setX((float)regAlpaca1X);
+        regularalpaca1.setY((float)regAlpacaY);
 
 
 
